667ce1f40a72120e7b01cc92658f44d93cc00442
bsrogers2 <bsrogers2@my.waketech.edu>
2022-03-07 19:33:13 -0500

    prod uri

--- API/app.py
+++ API/app.py
@@ -6,6 +6,6 @@ from models import db
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = ps.secret_key
-app.config['SQLALCHEMY_DATABASE_URI'] = ps.db_uri
+app.config['SQLALCHEMY_DATABASE_URI'] = ps.db_uri_production
 db.init_app(app)
 migrate = Migrate(app, db)
